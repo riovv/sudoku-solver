24b3a3f9e39f3f63a690730317fcfb16e4fbf2a4
Patric Nordmark <riosatiy@gmail.com>
2011-06-02 15:56:56 +0200

    Moved sudoku.clone() outside of the sudoku object, and renamed it to cloneObject()

--- sudoku.solver.js
+++ sudoku.solver.js
@@ -1,22 +1,12 @@
 /*jslint white: true, devel: true, onevar: true, browser: true, undef: true, nomen: true, regexp: true, continue: true, windows: true, newcap: true, maxerr: 50, indent: 4 */
 (function (global) {
+
     /*
         Create an unique namespace for sudoku solver.
-        And setup the puzzle's original state.
+        With CONSTANTS defining sudoku relationships.
     */
 	var sudoku = {
-
-        version: "0.1",
-        clone: function (obj) {
-            var i,
-                target = {};
-            for (i in obj) {
-                if (obj.hasOwnProperty(i)) {
-                    target[i] = obj[i];
-                }
-            }
-            return target;
-        },
+        VERSION: "0.5",
 
         SQUARES: ["A1", "A2", "A3", "A4", "A5", "A6", "A7", "A8", "A9", "B1", "B2", "B3", "B4", "B5", "B6", "B7", "B8", "B9", "C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "D1", "D2", "D3", "D4", "D5", "D6", "D7", "D8", "D9", "E1", "E2", "E3", "E4", "E5", "E6", "E7", "E8", "E9", "F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "G1", "G2", "G3", "G4", "G5", "G6", "G7", "G8", "G9", "H1", "H2", "H3", "H4", "H5", "H6", "H7", "H8", "H9", "I1", "I2", "I3", "I4", "I5", "I6", "I7", "I8", "I9"],
 
@@ -32,6 +22,8 @@
         UNITS_LENGTH: 3,
         PEERS_LENGTH: 20       
     };
+    
+
    
    
     sudoku.solver = {
@@ -115,7 +107,7 @@
     sudoku.solve = function (gridDigits) {
         // Get a new copy of the starting point.
         var s;
-        sudoku.solver.digits = sudoku.clone(sudoku.DIGITS);
+        sudoku.solver.digits = cloneObject(sudoku.DIGITS);
         
         // Try to assign digits from the grid.
         for (s in gridDigits) {
@@ -177,6 +169,23 @@
         for (i = 0; i < nNodes; i++) {
             nodeList[i].value = "";
         }
+    };
+     
+    /*
+        A utility functions to clone an Object.
+        Used by sudoku.solve() in order to clone the starting digits into 
+        a new instance.
+    */
+    var cloneObject = function (obj) {
+        var i,
+        target = {};
+        for (i in obj) {
+            if (obj.hasOwnProperty(i)) {
+                target[i] = obj[i];
+            }
+        }
+        
+        return target;
     };
    
     global.sudoku = sudoku;
