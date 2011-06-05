aa4184c9d313dbad10fad3b60be4e40ffe3abf35
Patric Nordmark <riosatiy@gmail.com>
2011-06-06 05:52:33 +0800

    Changed GUI button to accommodate the change on solve/run method.

--- sudoku.solver.js
+++ sudoku.solver.js
@@ -5,7 +5,7 @@
         Create an unique namespace for sudoku solver.
         With CONSTANTS defining sudoku relationships.
     */
-	var sudoku = {
+    var sudoku = {
         VERSION: "0.5",
 
         SQUARES: ["A1", "A2", "A3", "A4", "A5", "A6", "A7", "A8", "A9", "B1", "B2", "B3", "B4", "B5", "B6", "B7", "B8", "B9", "C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "D1", "D2", "D3", "D4", "D5", "D6", "D7", "D8", "D9", "E1", "E2", "E3", "E4", "E5", "E6", "E7", "E8", "E9", "F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "G1", "G2", "G3", "G4", "G5", "G6", "G7", "G8", "G9", "H1", "H2", "H3", "H4", "H5", "H6", "H7", "H8", "H9", "I1", "I2", "I3", "I4", "I5", "I6", "I7", "I8", "I9"],
@@ -138,8 +138,8 @@
         },
         
         /*
-            In a unit, if a digit can only be assigned in one square.
-            Then assign it there.
+            In a unit, if a digit only has one possible square to
+            which it can be assigned. Then assign it there.
         */
         assignSingleCandidate: function (digits) {
             var u, s, possible, unit, digit;
@@ -171,7 +171,7 @@
         },
         
         /*
-            Inside a square if all possible squares for a digit are in line horizontically or vertically.
+            Inside a box unit if all possible squares for a digit are in line horizontically or vertically.
             Even if it is impossible to know exactly where to assign the digit, we can eliminate the digit from
             all other squares in the horizontal/vertical unit of those squares.
             
